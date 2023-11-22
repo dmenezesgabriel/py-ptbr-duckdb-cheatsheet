285011f453663cde8202ebdb4fc5e054f0dd11dd
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-11-21 22:35:06 -0300

    Move marp related do docs dir

---
+++ docs/marp/scripts/export_slides.sh
@@ -0,0 +1,4 @@
+npx marp --engine engine.js slides.md \
+         --html \
+         --allow-local-files \
+         --output dist/presentation.html
--- scripts/export_slides.sh
+++
@@ -1,1 +0,0 @@
-npx marp --engine engine.js docs/slides.md --html --allow-local-files --output dist/presentation.html
